be4831c6aad1d6a97a7b7885581e17b7265bdb68
Code Warlock <dessymichaeltgp123@gmail.com>
2024-06-15 12:33:01 +0100

    Datatypes in JS

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+//Variables and Datatypes
+
+// console.log("Hi My name is Desmond!");
+// Javascript Basic Datatypes
+/*
+    Numbers.
+    Strings.
+    Boolean.
+    // Objects, Arrays, Undefined, Null, etc
+*/
+// typeof
+// console.log(12.4);
+// console.log(typeof 12.4);
+// Integers(Whole number);
+// Floats(Floating point number) === Decimal numbers;
+
+// console.log("Gbenga");
+// console.log(typeof "Gbenga");
+
+// console.log(true);
+// console.log(false);
