96b1714141cd72fd0129f1aad366988b92880403
Code Warlock <dessymichaeltgp123@gmail.com>
2024-06-15 13:03:22 +0100

    Variables

---
+++ assets/js/variables.js
@@ -0,0 +1,18 @@
+// Variables in JS
+
+console.log("Variables in JS!");
+// var declaration keyword, name of variable = value;
+let y = 23;
+let fm = "Famuyiwa Qazeem Hon. Chief Justice."
+let isAlive = true; //Camelcase
+// console.log(x);
+// console.log(fm);
+// console.log(isAlive);
+// console.log(typeof x);
+// console.log(typeof fm);
+// console.log(typeof isAlive);
+
+// console.log(x, fm)
+let peterAge = 23;
+let userName = prompt("Enter your name");
+console.log(userName);
